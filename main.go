03f9635d79b5f6a63615dbacb05d155becd73c40
Atch <atcheri@laposte.net>
2020-10-09 22:51:32 +0200

    un autre essai en utilisant le mapper

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 
+	// "github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
@@ -15,17 +16,23 @@ type ExpoWebhookPayload struct {
 	Platform    string `json:"platform"`
 }
 
-func HandleRequest(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	fmt.Printf("Processing request data for request %s.\n", request.RequestContext.RequestID)
-	fmt.Printf("Body size = %d.\n", len(request.Body))
-	fmt.Printf("Body content: %+v\n", request.Body)
+type ApiGatewayMap struct {
+	Body      ExpoWebhookPayload  `json:body`
+	Signature string              `json:signature`
+	Headers   map[string][]string `json:headers`
+}
+
+func HandleRequest(ctx context.Context, payload ApiGatewayMap) (events.APIGatewayProxyResponse, error) {
+	// fmt.Printf("Processing request data for request %s.\n", request.RequestContext.RequestID)
+	// fmt.Printf("Body size = %d.\n", len(payload.Body))
+	fmt.Printf("Body content: %+v\n", payload.Body)
 
 	fmt.Println("Headers:")
-	for key, value := range request.Headers {
+	for key, value := range payload.Headers {
 		fmt.Printf("    %s: %s\n", key, value)
 	}
 
-	return events.APIGatewayProxyResponse{Body: request.Body, StatusCode: 200}, nil
+	return events.APIGatewayProxyResponse{Body: "", StatusCode: 200}, nil
 }
 
 func main() {
