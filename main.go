a2c2f3c9f7bfd1ce6eec566dd1702de02d2faf1e
Atch <atcheri@laposte.net>
2020-10-09 22:27:43 +0200

    using api gateqway proxy

--- main.go
+++ main.go
@@ -4,15 +4,28 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-type MyEvent struct {
-	Name string `json:"name"`
+type ExpoWebhookPayload struct {
+	ID          string `json:"id"`
+	Status      string `json:"status"`
+	ArtifactUrl string `json:"artifactUrl"`
+	Platform    string `json:"platform"`
 }
 
-func HandleRequest(ctx context.Context, name MyEvent) (string, error) {
-	return fmt.Sprintf("Hello %s!", name.Name), nil
+func HandleRequest(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	fmt.Printf("Processing request data for request %s.\n", request.RequestContext.RequestID)
+	fmt.Printf("Body size = %d.\n", len(request.Body))
+	fmt.Printf("Body content: %+v\n", request.Body)
+
+	fmt.Println("Headers:")
+	for key, value := range request.Headers {
+		fmt.Printf("    %s: %s\n", key, value)
+	}
+
+	return events.APIGatewayProxyResponse{Body: request.Body, StatusCode: 200}, nil
 }
 
 func main() {
