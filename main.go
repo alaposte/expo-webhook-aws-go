2f8ee1d6624e5cdfd1f05d32ea0732cdae5448b3
Atch <atcheri@laposte.net>
2020-10-10 17:55:31 +0200

    trying APIGatewayV2HTTPRequest type as parameter

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"fmt"
 
-	// "github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
@@ -22,18 +21,19 @@ type ApiGatewayMap struct {
 	// Headers   map[string][]string `json:"headers"`
 }
 
-func HandleRequest(ctx context.Context, payload ApiGatewayMap) (events.APIGatewayProxyResponse, error) {
+func HandleRequest(ctx context.Context, request events.APIGatewayV2HTTPRequest) (events.APIGatewayV2HTTPResponse, error) {
 	// fmt.Printf("Processing request data for request %s.\n", request.RequestContext.RequestID)
-	// fmt.Printf("Body size = %d.\n", len(payload.Body))
-	// fmt.Printf("Body content: %+v\n", payload.Body)
-	fmt.Printf("signature value: %+v\n", payload.Signature)
+	// fmt.Printf("Body size = %d.\n", len(request.Body))
+	fmt.Printf("Body content: %+v\n", request.Body)
+	fmt.Printf("Headers: %+v\n", request.Headers)
+	// fmt.Printf("signature value: %+v\n", request.Signature)
 
 	// fmt.Println("Headers:")
-	// for key, value := range payload.Headers {
+	// for key, value := range request.Headers {
 	// 	fmt.Printf("    %s: %s\n", key, value)
 	// }
 
-	return events.APIGatewayProxyResponse{Body: "", StatusCode: 200}, nil
+	return events.APIGatewayV2HTTPResponse{Body: "", StatusCode: 200}, nil
 }
 
 func main() {
